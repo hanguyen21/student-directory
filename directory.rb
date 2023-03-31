52128acb26a52bae0ad94af0fd7cf93ac33ab35a
hanguyen <lotusviet.uk@gmail.com>
2023-03-31 13:02:15 +0100

    Add methods

--- directory.rb
+++ directory.rb
@@ -1,10 +1,27 @@
-puts "The students of Makers Academy"
-puts "-----------------"
-puts "Dr Hanisha Lecter"
-puts "Darth Vader"
-puts "Elon Musk"
-puts "Jonny Dan"
-puts "Steven Jonn"
-print "Overall, we have "
-print 5
-print " excellent students"
+
+students = [
+    "Dr Hanisha Lecter",
+    "Darth Vader",
+    "Elon Musk",
+    "Jonny Dan",
+    "Steven Jonn"
+]
+#print them
+def print_header
+  puts "The students of the Academy"
+  puts "-----------"
+end
+
+def print(names)
+    names.each do |name|
+        puts name 
+    end
+end 
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
