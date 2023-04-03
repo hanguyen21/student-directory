519e8e74d7622e14179e12a919b4e31cabba3037
hanguyen <lotusviet.uk@gmail.com>
2023-04-03 19:49:54 +0100

    adding an interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,27 @@
+def interactive_menu
+    students = []
+    loop do 
+        puts "1. Input the students"
+        puts "2. Show the students"
+        puts "9.Exit"
+        
+        selection = gets.chomp
+
+        case selection
+        when "1"
+            students = input_students
+        when "2"
+            print_header
+            print(students)
+            print_footer(students)
+        when "9"
+            exit    
+        else
+            puts "I don't get your answer, try again"
+        end 
+    end
+end
+
 def input_students
     puts "Please enter the names and cohorts"
     puts "To finish, just hit return twice"
@@ -18,7 +42,7 @@ def input_students
         students << {name: name, cohort: cohort}
         if students.count > 1
             puts "Now we have #{students.count} students" 
-        elsif student.count == 1 
+        elsif students.count == 1 
             puts "Now we have 1 student"   
         else
             break  
@@ -101,6 +125,7 @@ def print_footer(students)
     end
 end
 
+interactive_menu
 students = input_students
 print_header
 print(students)
