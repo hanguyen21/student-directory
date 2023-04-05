48d9167e78e8ab85ff3a939080c8ed1977fc7465
hanguyen <lotusviet.uk@gmail.com>
2023-04-05 11:35:03 +0100

    more refectoring

--- directory.rb
+++ directory.rb
@@ -1,139 +1,65 @@
-def interactive_menu
-    students = []
-    loop do 
-        puts "1. Input the students"
-        puts "2. Show the students"
-        puts "9.Exit"
-        
-        selection = gets.chomp
-
-        case selection
-        when "1"
-            students = input_students
-        when "2"
-            print_header
-            print(students)
-            print_footer(students)
-        when "9"
-            exit    
-        else
-            puts "I don't get your answer, try again"
-        end 
-    end
-end
+@students = []
 
 def input_students
-    puts "Please enter the names and cohorts"
+    puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-
-    students = []
-    #Define a default cohort value
-    default_cohort = :november
-
-    # Get the name from the user
-    puts "Enter the name of the student:"
     name = gets.chomp
     while !name.empty? do
-        puts "Enter the cohort of the student: "
-        cohort = gets.chomp
-
-        cohort = default_cohort if cohort.empty? || !is_valid_cohort?(cohort)
-        cohort = cohort.to_sym
-        students << {name: name, cohort: cohort}
-        if students.count > 1
-            puts "Now we have #{students.count} students" 
-        elsif students.count == 1 
-            puts "Now we have 1 student"   
-        else
-            break  
-        end
-
-        name = gets.chomp 
-    end
-    students
-end 
-
-def is_valid_cohort?(cohort)
-    valid_cohorts = %w[january february march april may june july august september october november december ]
-    valid_cohorts.include?(cohort.downcase)
-end
-def print_header
-    puts "The students of the Academy".center(60)
-    puts "------------".center(60)
-end
-
-def print(students)
-    students.each_with_index do |student,index|
-        puts "#{index+1}.#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-end
-
-def group(students)
-    cohorts = students.map {|student| student[:cohort]}
-    cohorts.each do |cohort|
-        puts "Students from the #{cohort} cohort: "
-        students.each do |student|
-            if student[:cohort] == cohort
-                puts "#{student[:name]} (#{student[:cohort]} cohort)"
-            end
-        end 
-        puts "-----------"
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
+        name = gets.chomp
     end 
+end
+
+def interactive_menu
+    loop do 
+      print_menu
+      process(gets.chomp)
+    end   
 end 
 
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
 
-def print_with_while_loop(students)
-   index = 0
-   while index < students.length
-    student = students[index]
-    puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort, hobbies #{student[:hobbies]}, country #{student[:country]}. )"
-    index += 1
-   end
-end 
+def show_students 
+    print_header
+    print_student_list
+    print_footer
+end
 
-def print_initial_names(students, initial)
-    initial_names = students.select { |student| student[:name][0].upcase == initial.upcase }
-    if initial_names.empty?
-        puts "No students found with initial names is #{initial}. "
+def process(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students
+    when "9"
+        exit
     else
-        puts " Students found with initial character #{initial} :"
-        initial_names.each do |student|
-            puts "#{student[:name]}".center(60)
-        end
+        puts "I don't know what you meant, try again"
     end
 end
 
-def print_short_names(students, max_length)
-    filter_students = students.select { |student| student[:name].length < max_length }
-    
-    if filter_students.empty?
-        puts "No students found with names shorter than #{max_length}. "    
-    else
-        puts "Students with names shorter than #{max_length}: "
-        filter_students.each do |student|
-            puts "#{student[:name]}".center(60)
-        end 
+def print_header
+    puts "The students of the Academy"
+    puts "---------------"
+end
+
+def print_student_list
+    @students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer(students)
-    if students.count > 1
-      puts "Overall, we have #{students.count} great students"
-    elsif students.count == 1
-      puts "Overal, we have 1 great student"
-    else
-    end
+def print_footer
+    puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
-students = input_students
-print_header
-print(students)
-group(students)
 
-initial = "h" 
-print_initial_names(students, initial)
 
-max_length = 12
-print_short_names(students, max_length)
-print_footer(students)
+
+
